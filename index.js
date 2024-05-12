6859408c36539765e4fef9a4482c27c2a5986897
Rahe <130391604+rahe01@users.noreply.github.com>
2024-05-12 12:07:50 +0600

    deploy

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
-app.use(cors({
-  origin: ['http://localhost:5173' , ],
-  credentials: true
-}));
+const corsOptions ={
+  origin: [ 'http://localhost:5173', 'https://food-09.web.app', 'https://food-09.firebaseapp.com'],
+
+  credentials: true,
+  optionSuccessStatus: 200,
+
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -57,13 +62,14 @@ const verifyJWT = (req, res, next) => {
 }
 
 
-
-
-
-
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const foodCollection = client.db("foodBunny").collection("food");
 
@@ -76,13 +82,7 @@ async function run() {
       console.log(req.cookies)
       const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN , { expiresIn: '1h' })
       res
-      .cookie('tokennn', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite:'none',
-        
-        
-      })
+      .cookie('tokennn', token, cookieOptions)
       .send({'success': true,})
     })
 
@@ -90,7 +90,7 @@ async function run() {
        const user = req.body
       console.log('logout')
        res
-       .clearCookie('tokennn', {maxAge:0})
+       .clearCookie('tokennn', {...cookieOptions , maxAge: 0})
        .send({'success': true,})
     })
 
@@ -238,7 +238,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
